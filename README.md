1115354ba1b37a1ad5c9a55b0563334ef79d4727
“Fernando <jjonathanvalle@gmail.com>
2025-03-20 11:23:15 -0300

    SQL_f

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# f_AGBD
+SELECT *
+FROM person
+WHERE address_street_name = 'Northwestern Dr'
+ORDER BY address_number DESC
+LIMIT 1
---
+++ mtry.sql
@@ -0,0 +1,5 @@
+/*SQL Mystery*/
+
+SELECT * FROM crime_scene_report
+WHERE date = 20180115 AND type = 'murder' AND city = 'SQL City';
+
